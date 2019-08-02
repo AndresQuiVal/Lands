8b2dce544e4a51f74b13ac02f02c28ac5db1d200
AndresQuiVal <53063747+AndresQuiVal@users.noreply.github.com>
2019-08-02 13:37:34 -0500

    Create Login Service with ApiService
    Create a correct login service for the application and add a Model class for reading the users

---
+++ Lands/Lands/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Lands.Models
+{
+    public class UserInfo // Model that provides the User Information
+    {
+        public string Email { get; set; }
+        public bool HasRegistered { get; set; }
+        public string LoginProvider { get; set; }
+    }
+}
--- Lands/Lands/ViewsModels/LoginViewModel.cs
+++ Lands/Lands/ViewsModels/LoginViewModel.cs
@@ -9,10 +9,17 @@ namespace Lands.ViewsModels
     using System.Windows.Input;
     using Xamarin.Forms;
     using GalaSoft.MvvmLight.Command;
+    using Lands.Services;
+    using Models;
     #endregion
 
     class LoginViewModel : BaseViewModel // Class that with allow us to change the value of the props in execution time of the app
     {
+        #region Services
+
+        ApiService apiService;
+        
+        #endregion
         #region Private class fields
         private string email;
         private string password;
@@ -55,6 +62,7 @@ namespace Lands.ViewsModels
         #region Constructors
         public LoginViewModel()
         {
+            apiService = new ApiService();
             IsEnabled = true;
         }
         #endregion
@@ -64,15 +72,40 @@ namespace Lands.ViewsModels
         {
             this.IsRunning = true;
             this.IsEnabled = false;
-            if (Email != "andres.quirozv14@gmail.com" || Password != "Valdovinos15")
+            Response internet_connection = await apiService.CheckConnection();
+            if (!internet_connection.IsSuccess)
             {
-                this.Email = string.Empty;
-                this.Password = string.Empty;
                 this.IsRunning = false;
                 this.IsEnabled = true;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
-                    "You've entered an invalid token",
+                    internet_connection.Message,
+                    "Accept");
+                return;
+            }
+            TokenResponse token = await apiService.GetToken(
+                "https://landsapi1.azurewebsites.net",
+                Email,
+                Password);
+            if (token == null)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Something was wrong... \ntry later please.",
+                    "Accept");
+                return;
+            }
+
+            if (token.AccessToken == null)
+            {
+                this.Password = "";
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    token.ErrorDescription,
                     "Accept");
                 return;
             }
